a92ea64cc4e52716f1de8d38464755af7913a26b
ansark <anzaku_m88@sky.707.to>
2017-03-29 19:22:56 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,22 @@ class UsersController < ApplicationController
   end
 
   def new
-    debugger
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"  #flashはRails固有のもの。
+      redirect_to @user  #redirect_to user_url(@user)と等価
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+    get signup_path
+    
+    #postした後にUser.countの値が変わらないことをテストする（userの作成に失敗する）
+    assert_no_difference 'User.count' do
+    post users_path, params: { user: { name:  "",
+                                       email: "user@invalid",
+                                       password:              "foo",
+                                       password_confirmation: "bar" } }
+    end
+    
+    assert_template 'users/new' #送信に失敗したときにnewアクションが再描画されるはず
+    
+    #エラーメッセージのテスト
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    assert_select 'div#error_explanation ul li',"Name can't be blank"
+    assert_select 'div#error_explanation ul li',"Email is invalid"
+    assert_select 'div#error_explanation ul li',"Password confirmation doesn't match Password"
+    assert_select 'div#error_explanation ul li',"Password is too short (minimum is 6 characters)"
+    
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    
+    #User.countがpostに後に1増えていたらテストOK
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!  #指定されたリダイレクト先に移動するメソッド
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert flash[:success] , "" #テストをパスする
+  end
+end
