a7cb434c39b15d5d96cf8815c1b5cff77bdab521
ansark <anzaku_m88@sky.707.to>
2017-04-06 22:47:27 +0000

    Implement base login and End Chapter8

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  post '/signup',  to: 'users#create'  #resourcesメソッドでusers#createはルーティング済なので必要ない？
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
+  #resourcesメソッドを使うとユーザーのURLを生成するための
+  #多数の名前付きルート と共に、RESTfulなUsersリソースで
+  #必要となるすべてのアクションが利用できるようになる
   resources :users
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -37,6 +37,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!  #指定されたリダイレクト先に移動するメソッド
     assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.empty?
     assert flash[:success] , "" #テストをパスする
   end
