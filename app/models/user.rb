a35705817b8f8cdc1860ee4668f0ab982fd9cc1d
ansark <anzaku_m88@sky.707.to>
2017-04-09 14:56:53 +0000

    Accept Cookie

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+ attr_accessor :remember_token
+
   before_save { self.email = email.downcase } #callback for uniqueness
   
   validates :name,  presence: true, length: { maximum: 50 }
@@ -9,7 +11,6 @@ class User < ApplicationRecord
                     
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-end
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -17,3 +18,26 @@ end
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+  
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64                                                                                                                                       
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+   # 渡されたトークンが保存してるダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
+end
